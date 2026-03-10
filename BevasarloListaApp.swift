b54945389be5d969522a740bf2dd256741bed56a
attilabekecs <attila.bekecs@icloud.com>
2026-03-10 09:12:55 +0100

    Create BevasarloListaApp.swift

---
+++ BevasarloListaApp.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import SwiftData
+
+@main
+struct BevasarloListaApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: [
+            ShoppingList.self,
+            ShoppingItem.self,
+            ProductPrice.self,
+            Recipe.self,
+            RecipeIngredient.self,
+            PantryItem.self
+        ])
+    }
+}
