8154c9b95c7a0c89ac6e530990fd2f63a816cc3d
attilabekecs <attila.bekecs@icloud.com>
2026-03-10 12:47:08 +0100

    Update ProductCategory.swift

--- ProductCategory.swift
+++ ProductCategory.swift
@@ -4,13 +4,18 @@ enum ProductCategory: String, CaseIterable, Codable, Identifiable {
 
     case vegetables = "Zöldség"
     case fruits = "Gyümölcs"
+    case bakery = "Pékáru"
     case meat = "Hús"
     case dairy = "Tejtermék"
-    case bakery = "Pékáru"
     case frozen = "Fagyasztott"
     case drinks = "Ital"
     case snacks = "Snack"
+
+    case pantry = "Alapanyag"
     case household = "Háztartás"
+    case baby = "Baba"
+    case pharmacy = "Gyógyszertár"
+
     case other = "Egyéb"
 
     var id: String { rawValue }
